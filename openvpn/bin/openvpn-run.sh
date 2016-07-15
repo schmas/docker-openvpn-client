ad71fb71757d0ebe1bea7de80117ad8f138a4bab
Diego Schmidt <dceschmidt@gmail.com>
2016-07-15 19:05:29 -0300

    fixing to openvpn call correct down script.

--- openvpn/bin/openvpn-run.sh
+++ openvpn/bin/openvpn-run.sh
@@ -5,5 +5,5 @@ echo "OpenVPN OPTS: ${OPENVPN_OPTS}"
 echo "OpenVPN config: ${OPENVPN_CONFIG}"
 exec openvpn --script-security 2 \
     --up "/etc/openvpn/bin/openvpn-up.sh" \
-    --down "/etc/openvpn/bin/openvpn-up.sh" \
+    --down "/etc/openvpn/bin/openvpn-down.sh" \
     $OPENVPN_OPTS --config "$OPENVPN_CONFIG"
